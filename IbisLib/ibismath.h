5ae3071b1ec9dfc885eb3f2145f1cf268affb584
anka_at_nist <pluszcz@gmail.com>
2017-02-28 12:51:05 -0500

    Split ibismath in 2 parts - in IbisLib no openCV, in IbisOpenCV renamed to ibisopencvmath, only functions using OpenCV. Changed class IbisMath to namespace IbisMath. The same namespace is used in both ibismath and ibisopencvmath.

---
+++ IbisLib/ibismath.h
@@ -0,0 +1,41 @@
+/*=========================================================================
+Ibis Neuronav
+Copyright (c) Simon Drouin, Anna Kochanowska, Louis Collins.
+All rights reserved.
+See Copyright.txt or http://ibisneuronav.org/Copyright.html for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+     PURPOSE.  See the above copyright notice for more information.
+=========================================================================*/
+#ifndef IBISMATH_H
+#define IBISMATH_H
+
+#include "SVL.h"
+#include <vector>
+
+class vtkMatrix4x4;
+
+namespace IbisMath
+{
+    // Scalar operations
+    static double Average( std::vector<double> & samples );
+    static double StdDev( std::vector<double> & samples, double average );
+    static double StdDev( std::vector<double> & samples );
+
+    // SVL vec operations
+    static Vec3 AverageVec3( std::vector<Vec3> & allVecs );
+    static Vec3 StdDevVec3( std::vector<Vec3> & allVecs );
+
+    // VTK mat 4 and SVL Vec3
+    static Vec3 Translation( vtkMatrix4x4 * mat );
+    static void MultMat4Vec3( vtkMatrix4x4 * mat, const Vec3 & in, Vec3 & out );
+    static Vec3 MultMat4Point3( vtkMatrix4x4 * mat, const Vec3 & in );
+
+    // VTK mat4 utilities
+    static vtkMatrix4x4 * DuplicateMat4( vtkMatrix4x4 * in );
+    static void ClearMat4Array( std::vector< vtkMatrix4x4 * > & allMats );
+
+};
+
+#endif
--- IbisOpenCV/ibisopencvmath.h
+++ IbisOpenCV/ibisopencvmath.h
@@ -8,8 +8,8 @@ See Copyright.txt or http://ibisneuronav.org/Copyright.html for details.
      the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
      PURPOSE.  See the above copyright notice for more information.
 =========================================================================*/
-#ifndef IBISMATH_H
-#define IBISMATH_H
+#ifndef IBISOPENCVMATH_H
+#define IBISOPENCVMATH_H
 
 #include "SVL.h"
 #include <vector>
@@ -17,28 +17,11 @@ See Copyright.txt or http://ibisneuronav.org/Copyright.html for details.
 
 class vtkMatrix4x4;
 
-class IbisMath
+namespace IbisMath
 {
-
-public:
-
-    // Scalar operations
-    static double Average( std::vector<double> & samples );
-    static double StdDev( std::vector<double> & samples, double average );
-    static double StdDev( std::vector<double> & samples );
-
-    // SVL vec operations
-    static Vec3 AverageVec3( std::vector<Vec3> & allVecs );
-    static Vec3 StdDevVec3( std::vector<Vec3> & allVecs );
-
     // cv::point operations
     static cv::Point3f AverageCvPoint3f( std::vector<cv::Point3f> & all );
     static cv::Point2f AverageCvPoint2f( std::vector<cv::Point2f> & all );
-
-    // VTK mat 4 and SVL Vec3
-    static Vec3 Translation( vtkMatrix4x4 * mat );
-    static void MultMat4Vec3( vtkMatrix4x4 * mat, const Vec3 & in, Vec3 & out );
-    static Vec3 MultMat4Point3( vtkMatrix4x4 * mat, const Vec3 & in );
 
     // VTK mat 4 and Cv::Point3f
     static cv::Point3f MultMat4CvPoint3f( vtkMatrix4x4 * mat, cv::Point3f & in );
@@ -46,11 +29,6 @@ public:
     // SVL vec and cv vec
     static cv::Point3f Vec3ToCVPoint3f( Vec3 & v );
     static Vec2 CVPoint2fToVec2( cv::Point2f & p );
-
-    // VTK mat4 utilities
-    static vtkMatrix4x4 * DuplicateMat4( vtkMatrix4x4 * in );
-    static void ClearMat4Array( std::vector< vtkMatrix4x4 * > & allMats );
-
 };
 
 #endif
