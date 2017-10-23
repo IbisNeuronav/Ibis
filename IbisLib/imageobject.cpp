1e73894dbc287f4a56c9701de319fcba3e89d6f0
anka_at_nist <pluszcz@gmail.com>
2017-10-23 14:35:26 -0400

    Added missing GetPointer() calls in ImageObject.

--- IbisLib/imageobject.cpp
+++ IbisLib/imageobject.cpp
@@ -99,7 +99,7 @@ ImageObject::ImageObject()
 
     // Watch volume properties to be able to render when properties change
     m_volumePropertyWatcher = vtkSmartPointer<vtkEventQtSlotConnect>::New();
-    m_volumePropertyWatcher->Connect( m_volumeProperty, vtkCommand::ModifiedEvent, this, SLOT(MarkModified()) );
+    m_volumePropertyWatcher->Connect( m_volumeProperty.GetPointer(), vtkCommand::ModifiedEvent, this, SLOT(MarkModified()) );
     m_volumePropertyWatcher->Connect( m_volumeProperty->GetScalarOpacity(), vtkCommand::ModifiedEvent, this, SLOT(MarkModified()) );
     m_volumePropertyWatcher->Connect( m_volumeProperty->GetGradientOpacity(), vtkCommand::ModifiedEvent, this, SLOT(MarkModified()) );
     m_volumePropertyWatcher->Connect( m_volumeProperty->GetRGBTransferFunction(), vtkCommand::ModifiedEvent, this, SLOT(MarkModified()) );
@@ -433,7 +433,7 @@ void ImageObject::UpdateVolumeRenderingParamsInMapper()
             mapper->SetFinalColorWindow( m_colorWindow );
             mapper->SetCroppingRegionPlanes( m_volumeRenderingBounds );
 
-            SetVolumeClippingEnabled( pv->volumeClippingWidget, !IsHidden() && m_showVolumeClippingBox );
+            SetVolumeClippingEnabled( pv->volumeClippingWidget.GetPointer(), !IsHidden() && m_showVolumeClippingBox );
         }
         ++it;
     }
@@ -533,7 +533,7 @@ void ImageObject::Setup3DRepresentation( View * view )
     boxRep->InsideOutOn();
     boxRep->OutlineCursorWiresOff();
 
-    m_volumeClippingBoxWatcher->Connect( volumeClippingWidget, vtkCommand::InteractionEvent, this, SLOT(OnVolumeClippingBoxModified(vtkObject*)) );
+    m_volumeClippingBoxWatcher->Connect( volumeClippingWidget.GetPointer(), vtkCommand::InteractionEvent, this, SLOT(OnVolumeClippingBoxModified(vtkObject*)) );
 
 
     // Add the actors to the map of instances we keep
@@ -555,10 +555,10 @@ void ImageObject::Release3DRepresentation( View * view )
         PerViewElements * perView = (*itAssociations).second;
 
         // remove outline
-		view->GetRenderer()->RemoveViewProp( perView->outlineActor );
+        view->GetRenderer()->RemoveViewProp( perView->outlineActor.GetPointer() );
 
         // remove volume
-        view->GetRenderer()->RemoveViewProp( perView->volume );
+        view->GetRenderer()->RemoveViewProp( perView->volume.GetPointer() );
 
         // remove volume clipping box
         perView->volumeClippingWidget->SetInteractor( 0 );
