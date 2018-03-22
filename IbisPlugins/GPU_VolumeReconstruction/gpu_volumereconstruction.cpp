c64d3a58f3d32873e9ff36f300889ea42edc578f
anka_at_nist <pluszcz@gmail.com>
2018-03-22 10:35:06 -0400

    Removed obsolete includes.

--- IbisPlugins/GPU_VolumeReconstruction/gpu_volumereconstruction.cpp
+++ IbisPlugins/GPU_VolumeReconstruction/gpu_volumereconstruction.cpp
@@ -1,19 +1,8 @@
 #include "gpu_volumereconstruction.h"
 #include "ibisitkvtkconverter.h"
-#include "scenemanager.h"
-#include "sceneobject.h"
-#include "usacquisitionobject.h"
-#include "vtkTransform.h"
-#include "vtkLinearTransform.h"
 #include "vtkMatrix4x4.h"
 
 #include "vtkImageData.h"
-#include "vtkImageShiftScale.h"
-#include "vtkImageLuminance.h"
-#include "vtkMath.h"
-
-#include "imageobject.h"
-
 
 GPU_VolumeReconstruction::GPU_VolumeReconstruction()
 {
