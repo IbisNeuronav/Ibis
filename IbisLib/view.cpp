a60f3348d25f6eef97093ec5ea89188d2f5464ee
anka_at_nist <pluszcz@gmail.com>
2018-09-17 10:09:03 -0400

    Reverted changes in view.cpp.

--- IbisLib/view.cpp
+++ IbisLib/view.cpp
@@ -520,24 +520,6 @@ void View::ReferenceTransformChanged()
     // We update transform only if it is a 2D view or if Manager says we follow 3D views as well
     if( this->Manager && ( this->GetType() != THREED_VIEW_TYPE || this->Manager->Is3DViewFollowingReferenceVolume() ) )
     {
-        this->ApplyReferenceTransform();
-        NotifyNeedRender();
-    }
-}
-
-void View::WindowStartsRendering()
-{
-    if( this->GetType() != THREED_VIEW_TYPE )
-    {
-        this->ApplyReferenceTransform();
-    }
-    this->Renderer->ResetCameraClippingRange();
-}
-
-void View::ApplyReferenceTransform()
-{
-    if( this->Manager )
-    {
         ImageObject * obj = this->Manager->GetReferenceDataObject();
         if( obj && this->Renderer )
         {
@@ -554,8 +536,15 @@ void View::ApplyReferenceTransform()
             // backup inverted current transform
             this->PrevViewingTransform->DeepCopy( refTransform->GetMatrix() );
             this->PrevViewingTransform->Invert();
+
+            NotifyNeedRender();
         }
     }
+}
+
+void View::WindowStartsRendering()
+{
+    this->Renderer->ResetCameraClippingRange();
 }
 
 void View::SetupAllObjects()
