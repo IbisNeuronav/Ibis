0425346d6627eb37852d04b83a5b96072377ea1c
Houssem Gueziri <hgueziri@users.noreply.github.com>
2020-07-24 11:02:47 -0400

    Correct spelling in plugins description

--- IbisPlugins/PRISMVolumeRender/declare-plugin.cmake
+++ IbisPlugins/PRISMVolumeRender/declare-plugin.cmake
@@ -1,1 +1,1 @@
-DeclarePlugin( PRISMVolumeRender YES DESCRIPTION “This plugins add a global object that can volume render multiple images with functionality to edit shaders in real-time“ )
+DeclarePlugin( PRISMVolumeRender YES DESCRIPTION "This plugins add a global object that can volume render multiple images with functionality to edit shaders in real-time" )
