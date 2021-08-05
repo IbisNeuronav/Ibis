a1e4dea9e5943b8db97d6cd5d03cce45c1b7ce2e
nist-anka <pluszcz@gmail.com>
2021-08-05 16:03:44 -0400

    Revert change in IbisHardwareIGSIO, added function ObjectAboutToBeRemovedFromScene() to trackedsceneobject.cpp/h.

--- IbisLib/trackedsceneobject.cpp
+++ IbisLib/trackedsceneobject.cpp
@@ -179,3 +179,15 @@ void TrackedSceneObject::InternalUpdateWorldTransform()
         m_uncalibratedWorldTransform->Concatenate( Parent->GetWorldTransform() );
     m_uncalibratedWorldTransform->Concatenate( m_transform );
 }
+
+void TrackedSceneObject::ObjectAboutToBeRemovedFromScene()
+{
+    // We mark the object as not driven by hardware, removing it from the list of tracked objects.
+    // We also mark it as not managed by tracker and not managed by system.
+    // We have to remember that the object is known to the SceneManager until it is removed from the list of all objects.
+    // Before the final removal from the list there are many callbacks that may access the object and we do not want it
+    // to be treated as a special object.
+    this->SetHardwareModule( nullptr );
+    this->SetObjectManagedBySystem( false );
+    this->SetObjectManagedByTracker( false );
+}
