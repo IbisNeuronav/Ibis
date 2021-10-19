d4c1a83adad8bc17d4580f872370a8a9470d2bcd
nist-anka <pluszcz@gmail.com>
2021-10-19 17:18:58 -0400

    ITK patch for lprintf, release 3.2.1 again.

--- IbisSuperBuild/IbisDeps/External_ITK.cmake
+++ IbisSuperBuild/IbisDeps/External_ITK.cmake
@@ -7,7 +7,6 @@ ExternalProject_Add( ${itk_name}
     INSTALL_COMMAND ""
     GIT_REPOSITORY https://github.com/IbisNeuronav/ITK.git
     GIT_TAG 2e610a9ca92c7363ab65d3f85baff95ae1fb54a6
-    GIT_TAG v${IBIS_ITK_LONG_VERSION}
     CMAKE_ARGS -DCMAKE_INSTALL_PREFIX:PATH=${external_project_dir}/${itk_name}/install
                -DCMAKE_OSX_SYSROOT:PATH=${CMAKE_OSX_SYSROOT}
                -DCMAKE_OSX_DEPLOYMENT_TARGET:STRING=${CMAKE_OSX_DEPLOYMENT_TARGET}
