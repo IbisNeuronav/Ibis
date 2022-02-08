5b27ad20e8b0020c65cfd19f05e8e0d245762b57
nist-anka <pluszcz@gmail.com>
2022-02-08 11:43:35 -0500

    Doxygen comments in worldobject.h.

--- IbisLib/worldobject.h
+++ IbisLib/worldobject.h
@@ -21,9 +21,12 @@ class PolyDataObject;
 
 /**
  * @class   WorldObject
- * @brief   It is also called Scene Root, as it is the topmpost obect in a scene and all other objects are its children.
+ * @brief   It is also called Scene Root, as it is the topmost obect in a scene and all other objects are its children.
  *
- *  WorldObject
+ *  There should be only one WorldObject in the scene. Most of its properties are fixed and user cannot change them.
+ *  From WorldObject settings one can control the axes, the cursor, camera view angle, background colors,
+ *  interaction style and update frequence.
+ *
  */
 class WorldObject : public SceneObject
 {
@@ -38,28 +41,54 @@ public:
     WorldObject();
     virtual ~WorldObject();
 
+    /** Add axes to visualize the current coordinates system.
+     *  Initially axes are created when when SceneManager is instantiated.
+    */
     void SetAxesObject( vtkSmartPointer<PolyDataObject> obj );
+    /** Return current axes object. */
     vtkSmartPointer<PolyDataObject> GetAxesObject() { return m_axesObject; }
 
-    // can't set name or allow hidding or delete this type of object
+    /** @name  Unchangeable parameters
+     *  @brief User can't set name or allow hidding or delete this type of object.
+     */
+    ///@{
+    /** Disable setting name. */
     virtual void SetName( QString name ) override {}
+    /** Disable changing name. */
     virtual void SetNameChangeable( bool c ) override {}
+    /** Disable hiding. */
     virtual void SetHidden( bool h ) { }
+    /** Disable hiding. */
     virtual void SetHidable( bool h ) override {}
+    /** Disable deleting. */
     virtual void SetObjectDeletable( bool d ) override {}
+    ///@}
 
+    /** Hide axes. */
     void SetAxesHidden( bool h );
+    /** Check axes visibility. */
     bool AxesHidden();
+    /** Decide if main 3D view is following the reference object. */
     void Set3DViewFollowsReferenceVolume( bool f );
+    /** Find out if main 3D view is following the reference object. */
     bool Is3DViewFollowingReferenceVolume();
+    /** Hide/show cursor. */
     void SetCursorVisible( bool v );
+    /** Check cursor visibility. */
     bool GetCursorVisible();
+    /** Set cursor color. */
     void SetCursorColor( const QColor & c );
+    /** Set cursor color. */
     void SetCursorColor( double[3] );
+    /** Get cursor color. */
     QColor GetCursorColor();
-    void SetBackgroundColor( const QColor & c ); // Set background color of all views
+    /** Set background color of all views, 3D included. */
+    void SetBackgroundColor( const QColor & c );
+    /** Get the color of all views, 3D may have different color. */
     QColor GetBackgroundColor();
-    void Set3DBackgroundColor( const QColor & c ); // Set background color of 3D view only
+    /** Set background color of 3D view only. */
+    void Set3DBackgroundColor( const QColor & c );
+    /** Get the color of 3D view. */
     QColor Get3DBackgroundColor();
 
     void Set3DInteractorStyle( InteractorStyle style );
