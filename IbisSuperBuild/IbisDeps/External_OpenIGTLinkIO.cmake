d76b6af0cf66a53fcdac3958fd5c9db593f2d8dc
errollgarner <17100565+errollgarner@users.noreply.github.com>
2023-03-24 09:53:45 -0400

    Update SuperBuild dependencies OIGTL/OIGTLIO
    * Update OpenIGTLinkIO to latest
    * Use a set version of OpenIGTLink instead of latest master to make updating it explicit in the future and avoid potential issues

--- IbisSuperBuild/IbisDeps/External_OpenIGTLinkIO.cmake
+++ IbisSuperBuild/IbisDeps/External_OpenIGTLinkIO.cmake
@@ -7,7 +7,7 @@ ExternalProject_Add( ${openigtlinkio_name}
   INSTALL_COMMAND ""
   GIT_REPOSITORY "https://github.com/IGSIO/OpenIGTLinkIO.git"
   #the tag is used to mark the version that compiles and links with the first version of ibis 4.x, it will have to be removed altogether once OpenIGTLinkIO is stable
-  GIT_TAG  30a04eb14e43b439178ba13130b7a2c1fb2b51c2
+  GIT_TAG  46975d197796063b956573f1b1022ac2e3643fe4
   CMAKE_ARGS -DCMAKE_OSX_SYSROOT:PATH=${CMAKE_OSX_SYSROOT}
              -DCMAKE_OSX_DEPLOYMENT_TARGET:STRING=${CMAKE_OSX_DEPLOYMENT_TARGET}
              -DCMAKE_BUILD_TYPE:STRING=${CMAKE_BUILD_TYPE}
